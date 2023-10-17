b7066802e21aa9bd6948d66e0d33bcc5c298fe91
thuanvu301103 <96073973+thuanvu301103@users.noreply.github.com>
2023-10-18 01:51:16 +0700

    updateupupdatedate hill climbing: Vu Ngoc Thuan

--- nqueens.py
+++ nqueens.py
@@ -6,7 +6,7 @@ class n_queens:
         # Number of queens 
         self.n = n
         # Initial state
-        self.initstate = [{}, {}, {}]
+        self.initstate = []
         # Backtracking
         self.visited = []
 
@@ -17,88 +17,60 @@ class n_queens:
                2[***]
                3[***]
         ''' 
-        len_1 = len(state[0])
-        #len_2 = len(state[1])
-        #len_3 = len(state[2])
-        new_state = copy.deepcopy(state)
-        #print(state)
-        row = x
-        cross_1 = x + (len(state[0]))
-        cross_2 = x + self.n-len(state[0])-1
-        if len_1 == 0: 
-            new_state[0].update({row:len(state)+1})
-            new_state[1].update({cross_1:len(state)+1})
-            new_state[2].update({cross_2:len(state)+1})
-            return new_state
-        #print("State: ", state)
-        
-
-        #row_curr = x
-        #cross_1_curr = row_curr + (len(new_state)-1)
-        #cross_2_curr = row_curr + (self.n-len(new_state))
-        #for i in range(1, len(new_state)):
-        #    row = state[i-1]
-        #    cross_1 = row + (i-1)
-        #    cross_2 = row + (self.n-i)
-        if row in state[0] or cross_1 in state[1] or cross_1 in state[2]:
-            return [{},{},{}]
-        new_state[0].update({row:len(state)+1})
-        new_state[1].update({cross_1:len(state)+1})
-        new_state[2].update({cross_2:len(state)+1})
+        new_state = state + [x]
+        if len(state) == 0: return new_state
+        row_curr = x
+        cross_1_curr = row_curr + (len(new_state)-1)
+        cross_2_curr = row_curr + (self.n-len(new_state))
+        for i in range(1, len(new_state)):
+            row = state[i-1]
+            cross_1 = row + (i-1)
+            cross_2 = row + (self.n-i)
+            if row_curr == row or cross_1_curr == cross_1 or cross_2_curr == cross_2:
+                    return []
         return new_state
     
     # Check solution
     def check_sol (self, state):
-        if len(state[0]) < self.n:
+        if len(state) < self.n:
             return False
         else:
-            row = set(state[0])
-            cross_1 = set(state[1])
-            cross_2 = set(state[2])
-            if len(row) != len(state[0]) or len(cross_1) != len(state[1]) or len(cross_2) != len(state[2]):
-                return False
-            return True
-
-            '''
             for i in range(1, self.n):
-                row = state[0]
+                row = state[i-1]
                 cross_1 = row + (i-1)
                 cross_2 = row + (self.n-i)
-
                 for j in range(i+1, self.n + 1):
                     row_curr = state[j-1]
-
                     cross_1_curr = row_curr + (j-1)
                     cross_2_curr = row_curr + (self.n-j)
-
-                    if row_curr == row or cross_1_curr == cross_1 or cross_2_curr == cross_2:
-                        #print ("Solution is False")
+                    if row_curr == row:
                         return False
+                    if cross_1_curr == cross_1:
+                        return False
+                    if cross_2_curr == cross_2:
+                        return False
+                        #print ("Solution is False")
+                        #return False
             #print ("Solution is True")
             return True
-            '''
 
 
 class dfs (n_queens): 
     def sol(self):
         stack = [self.initstate]
-        #print("Stack: ", stack, len(stack))
-        #self.visited = []
+        self.visited = []
         while len(stack) != 0:
-
             curr_state = stack.pop(-1)
-            #print("Stack: ", stack, len(stack))
-            #if self.check_sol(curr_state): return curr_state[0]
-            if len(curr_state[0]) == self.n: return curr_state[0]
+            #if self.check_sol(curr_state): return curr_state
+            if len(curr_state) == self.n: return curr_state
             val = [i for i in range(1, self.n+1)] 
             random.shuffle(val)
             #print(val)
             for i in val:
                 new_state = self.place_queen(curr_state, i)
-                #print(new_state)
-                if len(new_state[0]) == 0: continue
+                if new_state == []: continue
                 stack += [new_state]
-            #self.visited += [curr_state]
+            self.visited += [curr_state]
         #if self.check_sol(curr_state): return curr_state
         print ("No solution")
         return []
@@ -108,5 +80,47 @@ class dfs (n_queens):
 class brfs (n_queens):
    pass
 
-class heuristic:
-    pass
+class hillclimbing:
+    def __init__ (self, n):
+        self.n = n
+        self.initstate = []
+        for i in range(self.n):
+            self.initstate += [random.randint(1, self.n)]
+    def h(self, state):
+        h = 0
+        for i in range(1, self.n):
+            row = state[i-1]
+            cross_1 = row + (i-1)
+            cross_2 = row + (self.n-i)
+            for j in range(i+1, self.n + 1):
+                row_curr = state[j-1]
+                cross_1_curr = row_curr + (j-1)
+                cross_2_curr = row_curr + (self.n-j)
+                if row_curr == row: h += 1
+                if cross_1_curr == cross_1: h += 1
+                if cross_2_curr == cross_2: h += 1
+        return h
+    def sol(self):
+        curr_state = self.initstate
+        h = self.h(curr_state)
+        while h != 0:
+            lst = []
+            for i in range(1, self.n+1):
+                for j in range (1, self.n+1):
+                    new_state = copy.copy(curr_state)
+                    if curr_state[i-1] == j: continue
+                    new_state[i-1] = j
+                    new_h = self.h(new_state)
+                    if new_h <= h:
+                        if new_h == h: lst += [new_state]
+                        if new_h < h: 
+                            lst = [new_state]
+                            h = new_h
+            if lst == []: 
+                print ("No solution, try again")
+                return []
+            curr_state = random.choice(lst)
+        return curr_state
+ 
+
+    
